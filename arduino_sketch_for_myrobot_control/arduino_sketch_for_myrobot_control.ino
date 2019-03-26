4fdcbc0a01541e805ee6d06f0e403e9d6d560f6a
qq95538 <190808149@qq.com>
2019-03-26 17:20:24 +0800

    adjust the map() function to correct the servo accuracy.
    map(long, long, long, long, long)
     3.14 < msg.position[i] <3.14 which could be cut to a 0, 1 or 2 if not  * 1000

--- arduino_sketch_for_myrobot_control/arduino_sketch_for_myrobot_control.ino
+++ arduino_sketch_for_myrobot_control/arduino_sketch_for_myrobot_control.ino
@@ -9,32 +9,27 @@
 #include <ros.h>
 #include <sensor_msgs/JointState.h>
 #include <std_msgs/Float64.h>
-#include <Servo.h>
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  500 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVONUM 7
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-
-
 ros::NodeHandle  nh;
-Servo joint1, joint2;  // create servo object to control a servo
 
 void messageCb(const sensor_msgs::JointState& msg){
   digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  uint16_t pulselength1, pulselength2, pulselength3;
-  pulselength1 = map(msg.position[0], -3.14, 3.14, SERVOMIN, SERVOMAX);
-  pulselength2 = map(msg.position[1], -3.14, 3.14, SERVOMIN, SERVOMAX); 
-  pulselength3 = map(msg.position[2], -3.14, 3.14, SERVOMIN, SERVOMAX);
-  pwm.setPWM(0, 0, pulselength1);
-  pwm.setPWM(1, 0, pulselength2);
-  pwm.setPWM(2, 0, pulselength3);
+  uint16_t pulselength[SERVONUM];
+  for(int i = 0; i < SERVONUM; i++){
+    pulselength[i] = map(msg.position[i] * 1000, -1.57 * 1000, 1.57 * 1000, SERVOMIN, SERVOMAX);
+    pwm.setPWM(i, 0, pulselength[i]);
+  }  
 }
 
-ros::Subscriber<sensor_msgs::JointState> sub("joint_states", &messageCb );
+ros::Subscriber<sensor_msgs::JointState> sub("/joint_states", &messageCb );
 
 void setup()
 { 
@@ -46,14 +41,16 @@ void setup()
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
   delay(10);
 
-  //joint1.attach(A6);  // attaches the servo on pin to the servo object
-  //joint2.attach(A7);
-  //joint1.write(0);
-  //joint2.write(0);
+  digitalWrite(13, 1);
+  uint16_t pulselength[SERVONUM];
+  for(int i = 0; i < SERVONUM; i++){
+    pwm.setPWM(i, 0, (SERVOMIN+SERVOMAX)/2);
+  }
+
 }
 
 void loop()
-{  
+{ 
   nh.spinOnce();
   delay(1);
 }
